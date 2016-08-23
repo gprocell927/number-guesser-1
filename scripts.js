d8b5ca43b79992365f81b5fdd20e893e583d584a
Nick <nicknchambers@gmail.com>
2016-08-23 16:09:59 -0600

    Added random number generator, formatted reset button, added a range for answers, added high/low messages, and that's it.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,41 @@
 var userGuess = document.querySelector('#user-guess');
 var guessButton = document.querySelector('#guess');
 var clearButton = document.querySelector('#clear');
-var displayGuess = document.querySelector('#display-guess')
+var displayGuess = document.querySelector('#display-guess');
+var randomNumber = randomNumberGenerator();
+var displayMessage = document.querySelector('#display-message');
+var resetButton = document.querySelector('#reset');
 
+function randomNumberGenerator(){
+  return parseInt(Math.random() * (10 - 1) + 1);
+}
+
+function guessMessage () {
+  var num = parseInt(userGuess.value);
+  if (isNaN(num)) {
+    return displayMessage.innerText = "No."
+  }
+  if (randomNumber === num)  {
+    displayMessage.innerText = "You got it!"
+  }
+  else if (randomNumber < num) {
+    displayMessage.innerText = "Sorry, that guess is too high. Try a lower number."
+  }
+  else {
+    displayMessage.innerText = "Sorry, that guess is too low. Try a higher number."
+  }
+};
 //we need an event listener for clicking on the guess button
 
 
 // the "guess" button that will trigger displayUserGuess function to change userInput p tag to the userGuess
-guessButton.addEventListener('click',
-  function () {
-    debugger;
+guessButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
     var userGuessValue = getUserGuessValue();
     changeGuess(userGuessValue);
+    guessMessage();
+
   });
 
 //.innerText = userGuess
@@ -34,3 +58,8 @@ clearButton.addEventListener('click',
   function () {
     clearField();
   });
+
+resetButton.addEventListener('click',function () {
+  clearField();
+  randomNumber = randomNumberGenerator();
+});
