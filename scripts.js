920270670a180c247efaae49e572fb950c24113d
Gabi Procell <gprocell927@gmail.com>
2016-08-24 21:38:19 -0600

    fixed clear button. Almost have the user min and max set buttons working. Random number generator will not generate within user min and max range

--- scripts.js
+++ scripts.js
@@ -1,53 +1,38 @@
 var userGuess = document.querySelector('#user-guess');
 var guessButton = document.querySelector('#guess');
-var clearButton = document.querySelector('#clear');
-var displayGuess = document.querySelector('#display-guess');
-var randomNumber = randomNumberGenerator();
 var displayMessage = document.querySelector('#display-message');
+var displayGuess = document.querySelector('#display-guess');
+
+var clearButton = document.querySelector('#clear');
+var randomNumber = randomNumberGenerator();
 var resetButton = document.querySelector('#reset');
-var minButton = document.querySelector('#inputMin');
-var maxButton = document.querySelector('#inputMax');
 
-minButton.addEventListener('click', function (e) {
-
-    var userMinValue = inputMin.value;
-    userGuess.min = userMinValue;
-    resetButton.disabled = false;
-  });
-
-  maxButton.addEventListener('click', function (e) {
-
-      var userMaxValue = inputMax.value;
-      userGuess.max = userMaxValue;
-      resetButton.disabled = false;
-    });
-
+var minButton = document.querySelector('#minButton');
+var maxButton = document.querySelector('#maxButton');
+var minSet = document.querySelector('#inputMin');
+var maxSet = document.querySelector('#inputMax');
 
 
 function randomNumberGenerator(){
-  return parseInt(Math.random() * (userGuess.max - userGuess.min) + 1);
-}
+  return parseInt(Math.random() * (parseInt(userGuess.max) - parseInt(userGuess.min)) + 1);
+};
 
 function guessMessage () {
   var num = parseInt(userGuess.value);
   if (isNaN(num)) {
     return displayMessage.innerText = "No. Try again."
-  }
+  };
   if (randomNumber === num)  {
     displayMessage.innerText = "You got it!"
   }
-
   else if (num <= 0 || num > 100) {
     displayMessage.innerText = "Sorry, that number is not in the available range."
   }
-
   else if (randomNumber < num) {
     displayMessage.innerText = "Sorry, that guess is too high. Try a lower number."
   }
-
   else {
     displayMessage.innerText = "Sorry, that guess is too low. Try a higher number."
-
   }
 };
 
@@ -64,41 +49,56 @@ guessButton.addEventListener('click', function (e) {
     };
 
     resetButton.disabled = false;
-
   });
 
+  function changeGuess(text) {
+    displayGuess.innerText = text;
+  };
 
-function changeGuess(text) {
-  displayGuess.innerText = text;
-}
+  function getUserGuessValue () {
+    return userGuess.value;
+  };
 
-function getUserGuessValue () {
-  return userGuess.value;
-}
+  function nextLevel() {
+      userGuess.max = parseInt(userGuess.max) + 10;
+      userGuess.min = parseInt(userGuess.min) - 10;
+  };
 
-function clearField (){
-  userGuess.value = ''
-}
+  if (userGuess.value === "") {
+    clearButton.disabled = true;
+  };
+
+minButton.addEventListener('click', function (e){
+    e.preventDefault();
+    userGuess.min = parseInt(minSet.value);
+    resetButton.disabled = false;
+  });
+
+maxButton.addEventListener('click', function (e){
+    e.preventDefault();
+    userGuess.max = parseInt(maxSet.value);
+    resetButton.disabled = false;
+  });
+
+  function userSetRange(){
+    userGuess.max = parseInt(maxSet.value);
+    userGuess.min = parseInt(minSet.value);
+  };
 
 clearButton.addEventListener('click',
-  function () {
+  function (e) {
+    e.preventDefault();
     clearField();
   });
+
+  function clearField (){
+    userGuess.value = ''
+  };
 
 resetButton.addEventListener('click',function () {
   clearField();
   randomNumber = randomNumberGenerator();
   });
-
-
-function nextLevel() {
-    userGuess.max = parseInt(userGuess.max) + 10;
-    userGuess.min = parseInt(userGuess.min) - 10;
-};
-
-if (userGuess.value === "") {
-  clearButton.disabled = true;
-}
 
 if (userGuess.value === "" && displayMessage.innerText === "") {
   resetButton.disabled = true;
